c7aec580e398f1ac6edd937430e9befb326a5026
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-08-26 02:46:36 +0200

    fix colorschemes and separator

--- plugin/tabline/component.lua
+++ plugin/tabline/component.lua
@@ -6,13 +6,14 @@ local colors = config.colors
 
 local M = {}
 
+local reset = "ResetAttributes"
 local left_section_separator = { Text = opts.options.section_separators.left }
 local right_section_separator = { Text = opts.options.section_separators.right }
 local left_component_separator = { Text = opts.options.component_separators.left }
 local right_component_separator = { Text = opts.options.component_separators.right }
 
 local attributes_a, attributes_b, attributes_c = {}, {}, {}
-local component_seperator_attributes_b, component_seperator_attributes_c = {}, {}
+local component_seperator_attributes_a, component_seperator_attributes_b, component_seperator_attributes_c = {}, {}, {}
 local tabline_a, tabline_b, tabline_c, tabline_x, tabline_y, tabline_z = {}, {}, {}, {}, {}, {}
 
 local function create_attributes(window)
@@ -23,20 +24,24 @@ local function create_attributes(window)
 		{ Attribute = { Intensity = "Bold" } },
 	}
 	attributes_b = {
-		{ Foreground = { Color = colors[mode].b.fg or colors[mode].a.fg } },
-		{ Background = { Color = colors[mode].b.bg or colors[mode].a.bg } },
+		{ Foreground = { Color = colors[mode].b.fg } },
+		{ Background = { Color = colors[mode].b.bg } },
 	}
 	attributes_c = {
-		{ Foreground = { Color = colors[mode].c.fg or colors[mode].a.fg } },
-		{ Background = { Color = colors[mode].c.bg or colors[mode].a.bg } },
+		{ Foreground = { Color = colors[mode].c.fg } },
+		{ Background = { Color = colors[mode].c.bg } },
+	}
+	component_seperator_attributes_a = {
+		{ Foreground = { Color = colors[mode].a.bg } },
+		{ Background = { Color = colors[mode].b.bg } },
 	}
 	component_seperator_attributes_b = {
-		{ Foreground = { Color = colors[mode].b.fg or colors[mode].a.fg } },
-		{ Background = { Color = colors[mode].b.bg or colors[mode].a.bg } },
+		{ Foreground = { Color = colors[mode].b.bg } },
+		{ Background = { Color = colors[mode].c.bg } },
 	}
 	component_seperator_attributes_c = {
-		{ Foreground = { Color = colors[mode].b.bg or colors[mode].a.bg } },
-		{ Background = { Color = colors[mode].b.fg or colors[mode].a.fg } },
+		{ Foreground = { Color = colors[mode].a.bg } },
+		{ Background = { Color = colors[mode].c.bg } },
 	}
 end
 
@@ -54,10 +59,12 @@ local function extract_components(components_opts, window)
 				table.insert(components, { Text = " " .. v .. " " })
 			end
 		elseif type(v) == "table" and type(v[1]) == "string" then
-			local ok, result = pcall(require, "tabline." .. v[1])
+			local ok, result = pcall(require, "tabline.components." .. v[1])
 			if ok then
 				if type(v.fmt) == "function" then
-					table.insert(components, { Text = " " .. v.fmt(result, window) .. " " })
+					table.insert(components, { Text = " " .. v.fmt(result(window), window) .. " " })
+				else
+					table.insert(components, { Text = " " .. result(window) .. " " })
 				end
 			end
 		elseif type(v) == "function" then
@@ -72,10 +79,10 @@ end
 local function insert_section_separators(components, is_left)
 	local i = 1
 	while i <= #components do
-		-- if type(components[i]) == "table" and components[i].Text then
-		-- 	table.insert(components, i + 1, is_left and left_section_separator or right_section_separator)
-		-- 	i = i + 1
-		-- end
+		if type(components[i]) == "table" and components[i].Text and i < #components then
+			table.insert(components, i + 1, is_left and left_section_separator or right_section_separator)
+			i = i + 1
+		end
 		i = i + 1
 	end
 	return components
@@ -92,34 +99,68 @@ end
 
 local function right_component()
 	local result = {}
-	util.insert_elements(result, attributes_c)
 	if #tabline_x > 0 then
+		util.insert_elements(result, attributes_c)
 		util.insert_elements(result, tabline_x)
+		table.insert(result, reset)
 	end
-	util.insert_elements(result, attributes_b)
 	if #tabline_y > 0 then
-		util.insert_elements(result, tabline_y)
+		util.insert_elements(result, component_seperator_attributes_b)
+		table.insert(result, right_component_separator)
+		table.insert(result, reset)
 	end
-	util.insert_elements(result, attributes_a)
+	if #tabline_y > 0 then
+		util.insert_elements(result, attributes_b)
+		util.insert_elements(result, tabline_y)
+		table.insert(result, reset)
+	end
+	if #tabline_z > 0 and #tabline_y > 0 then
+		util.insert_elements(result, component_seperator_attributes_a)
+		table.insert(result, right_component_separator)
+		table.insert(result, reset)
+	elseif #tabline_z > 0 and #tabline_x > 0 then
+		util.insert_elements(result, component_seperator_attributes_c)
+		table.insert(result, right_component_separator)
+		table.insert(result, reset)
+	end
 	if #tabline_z > 0 then
+		util.insert_elements(result, attributes_a)
 		util.insert_elements(result, tabline_z)
+		table.insert(result, reset)
 	end
 	return result
 end
 
 local function left_component()
 	local result = {}
-	util.insert_elements(result, attributes_a)
 	if #tabline_a > 0 then
+		util.insert_elements(result, attributes_a)
 		util.insert_elements(result, tabline_a)
+		table.insert(result, reset)
 	end
-	util.insert_elements(result, attributes_b)
+	if #tabline_a > 0 and #tabline_b > 0 then
+		util.insert_elements(result, component_seperator_attributes_a)
+		table.insert(result, left_component_separator)
+		table.insert(result, reset)
+	elseif #tabline_a > 0 and #tabline_c > 0 then
+		util.insert_elements(result, component_seperator_attributes_c)
+		table.insert(result, left_component_separator)
+		table.insert(result, reset)
+	end
 	if #tabline_b > 0 then
+		util.insert_elements(result, attributes_b)
 		util.insert_elements(result, tabline_b)
+		table.insert(result, reset)
 	end
-	util.insert_elements(result, attributes_c)
+	if #tabline_b > 0 then
+		util.insert_elements(result, component_seperator_attributes_b)
+		table.insert(result, left_component_separator)
+		table.insert(result, reset)
+	end
 	if #tabline_c > 0 then
+		util.insert_elements(result, attributes_c)
 		util.insert_elements(result, tabline_c)
+		table.insert(result, reset)
 	end
 	return result
 end
--- plugin/tabline/config.lua
+++ plugin/tabline/config.lua
@@ -8,9 +8,9 @@ M.colors = {}
 
 local function mode_fmt(name)
 	local mode_text = {
-		normal_mode = "WINDOW " + wezterm.nerdfonts.md_dock_window,
-		search_mode = "FIND " + wezterm.nerdfonts.oct_search,
-		copy_mode = "YANK " + wezterm.nerdfonts.md_content_copy,
+		normal_mode = "WINDOW " .. wezterm.nerdfonts.md_dock_window .. "",
+		search_mode = "FIND " .. wezterm.nerdfonts.oct_search .. "",
+		copy_mode = "YANK " .. wezterm.nerdfonts.md_content_copy .. "",
 	}
 
 	return mode_text[name]
@@ -31,8 +31,8 @@ local default_opts = {
 		color_overrides = {},
 	},
 	sections = {
-		tabline_a = { "mode" },
-		tabline_b = {},
+		tabline_a = { { "mode", fmt = mode_fmt } },
+		tabline_b = { "workspace" },
 		tabline_c = {},
 		tabline_x = {},
 		tabline_y = { "workspace" },
@@ -41,14 +41,34 @@ local default_opts = {
 	extensions = {},
 }
 
+local function lighten_color(color, percent)
+	local r = tonumber(color:sub(2, 3), 16)
+	local g = tonumber(color:sub(4, 5), 16)
+	local b = tonumber(color:sub(6, 7), 16)
+
+	r = math.floor(r + ((255 - r) * percent))
+	g = math.floor(g + ((255 - g) * percent))
+	b = math.floor(b + ((255 - b) * percent))
+
+	return string.format("#%02x%02x%02x", r, g, b)
+end
+
 local function get_colors(theme)
 	local scheme = wezterm.color.get_builtin_schemes()[theme]
-	local mantle = scheme["tab_bar"].inactive_tab["bg_color"]
+
+	local mantle = scheme.background
+	local surface0 = lighten_color(scheme.background, 0.1)
 	local blue = scheme["ansi"][5]
-	local surface0 = scheme["tab_bar"]["inactive_tab_edge"]
 	local text = scheme.foreground
-	local yellow = scheme["ansi"][3]
-	local green = scheme["ansi"][2]
+	local yellow = scheme["ansi"][4]
+	local green = scheme["ansi"][3]
+
+	if scheme["tab_bar"] then
+		if scheme["tab_bar"].inactive_tab then
+			mantle = scheme["tab_bar"].inactive_tab["bg_color"] or mantle
+			surface0 = scheme["tab_bar"]["inactive_tab_edge"] or surface0
+		end
+	end
 
 	return {
 		normal_mode = {
@@ -56,8 +76,16 @@ local function get_colors(theme)
 			b = { fg = blue, bg = surface0 },
 			c = { fg = text, bg = mantle },
 		},
-		copy_mode = { a = { fg = mantle, bg = yellow } },
-		search_mode = { a = { fg = mantle, bg = green } },
+		copy_mode = {
+			a = { fg = mantle, bg = yellow },
+			b = { fg = yellow, bg = surface0 },
+			c = { fg = text, bg = mantle },
+		},
+		search_mode = {
+			a = { fg = mantle, bg = green },
+			b = { fg = green, bg = surface0 },
+			c = { fg = text, bg = mantle },
+		},
 	}
 end
 
