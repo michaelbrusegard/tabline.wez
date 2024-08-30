cb505f9d291f43e07f5b40b1a24d8edabe421c00
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-08-30 15:27:11 +0200

    window component default

--- plugin/tabline/components/window/window.lua
+++ plugin/tabline/components/window/window.lua
@@ -5,7 +5,7 @@ return {
     icon = wezterm.nerdfonts.cod_window,
   },
   update = function(window)
-    local window_name = window:mux_window():get_title() or 'default'
-    return window_name
+    local window_name = window:mux_window():get_title()
+    return window_name == '' and 'default' or window_name
   end,
 }
