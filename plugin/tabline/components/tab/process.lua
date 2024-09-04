6f4304ab95021de9d79f03a9bc37102d51dc9c97
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-09-04 13:00:29 -0400

    fallback to title before domain

--- plugin/tabline/components/tab/process.lua
+++ plugin/tabline/components/tab/process.lua
@@ -8,15 +8,17 @@ return {
       foreground_process_name = foreground_process_name:match('([^/\\]+)[/\\]?$') or foreground_process_name
     end
 
-    -- fallback to the domain name if it is a non-local domain
-    if foreground_process_name == '' then
-      foreground_process_name = tab.active_pane.domain_name ~= 'local' and tab.active_pane.domain_name or ''
-    end
-
     -- fallback to the title if the foreground process name is unavailable
+    -- Wezterm uses OSC 1/2 escape sequences to guess the process name and set the title
+    -- see https://wezfurlong.org/wezterm/config/lua/pane/get_title.html
     -- title defaults to 'wezterm' if another name is unavailable
     if foreground_process_name == '' then
       foreground_process_name = (tab.tab_title and #tab.tab_title > 0) and tab.tab_title or tab.active_pane.title
+    end
+
+    -- if the tab active pane contains a non-local domain, use the domain name
+    if foreground_process_name == 'wezterm' then
+      foreground_process_name = tab.active_pane.domain_name ~= 'local' and tab.active_pane.domain_name or 'wezterm'
     end
 
     return foreground_process_name
