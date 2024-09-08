f93f141fe921c47762effb49e42205fb37b103ba
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-09-08 03:17:34 +0200

    fix key table nil check

--- plugin/tabline/components/window/mode.lua
+++ plugin/tabline/components/window/mode.lua
@@ -1,6 +1,6 @@
 return {
   update = function(window)
-    local key_table = window:active_key_table() or 'normal_mode'
+    local key_table = (window and window.active_key_table and window:active_key_table()) or 'normal_mode'
     local mode = key_table:gsub('_mode', ''):upper()
     return mode
   end,
