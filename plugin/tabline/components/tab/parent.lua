8d66cb65b1d8471bf312523db0f655a3f38e9d59
Andrii Zhovtiak <andy@urlog.net>
2025-01-04 15:19:00 +0200

    Improve "Parent" component for Windows paths.

--- plugin/tabline/components/tab/parent.lua
+++ plugin/tabline/components/tab/parent.lua
@@ -6,6 +6,10 @@ return {
     local cwd_uri = tab.active_pane.current_working_dir
     if cwd_uri then
       local file_path = cwd_uri.file_path
+      -- Replace backslashes with forward slashes for consistency
+      file_path = file_path:gsub('\\', '/')
+      -- Remove any leading and trailing slashes
+      file_path = file_path:match('^/*(.-)/*$')
       parent = file_path:match('([^/]*)/[^/]*$')
       if parent and #parent > opts.max_length then
         parent = parent:sub(1, opts.max_length - 1) .. '…'
